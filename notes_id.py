e767dde71d2b47af03f89ae40bb4cb0411194349
Dorian Kisfaludi <drk2150@columbia.edu>
2021-12-09 16:06:19 -0500

    saved

--- notes_id.py
+++ notes_id.py
@@ -1,3 +1,17 @@
 import read_audio_file
-from scipy import signal as dsp
+from scipy import signal as ssp
 
+def compute_stft(audio): # Length of segments as input ?
+    """
+    Input : AudioFile object
+    Outputs :
+        * Array of sample frequencies
+        * Array of segment times
+        * STFT of AudioFile.signal 
+    """
+    return ssp.stft(
+        audio.signal,
+        audio.sampling_freq,
+        # WINDOW ?,
+        # LENGTH OF SEGMENTS
+    )
--- read_audio_file.py
+++ read_audio_file.py
@@ -5,4 +5,13 @@ class AudioSignal:
     Audio signal object
     """
     def __init__(self, filepath):
-        self.rate, self.signal = wav.read(filepath)
+        ate, self.signal = wav.read(filepath)
+        self.length = len(self.signal)
+        self.sampling_freq = 1/rate
+        self.total_time_duration = self.length/rate
+
+
+#%% Main
+
+filepath = input('Please enter path of file to process: ')
+audio = AudioSignal()
